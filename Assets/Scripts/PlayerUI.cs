93f442a12c9edf6c20c180369bf64b9e23f58e81
Keith Austin <datketh@gmail.com>
2020-03-10 12:07:37 -0400

    Fix PlayerUI

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HUD : MonoBehaviour
+public class PlayerUI : MonoBehaviour
 {
+    private const int ORDERS_BAR_WIDTH = 100, RESOURCE_BAR_HEIGHT = 40;
+    private Player player;
+
     public GUISkin resourceSkin, ordersSkin;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = transform.root.GetComponent<Player>();
     }
 
     // Update is called once per frame
-    void Update()
+    void OnGUI()
     {
-        
+        if (player && player.human)
+        {
+            DrawOrdersBar();
+            DrawResourceBar();
+        }
+    }
+
+    private void DrawOrdersBar ()
+    {
+
+    }
+
+    private void DrawResourceBar ()
+    {
+
     }
 }
