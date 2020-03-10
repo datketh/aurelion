1c132b14e4807f9c423830077e189979c576aab1
Keith Austin <datketh@gmail.com>
2020-03-10 12:05:52 -0400

    Create PlayerUI script

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    public GUISkin resourceSkin, ordersSkin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
